242a831d083e7d56b633082b8bb4c4d89818ebf7
harinder007 <harinderyadav1008@gmail.com>
2023-04-14 18:25:31 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const home = require('./routes/home')
+
+
+const app = express();
+app.use(express.json());
+
+app.use("/home",home)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{console.log(`Listening to port ${port}`)})
