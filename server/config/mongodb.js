0681faebcacd9169cc14cd638144fba0c42b1264
harinder007 <harinderyadav1008@gmail.com>
2023-04-14 19:01:34 +0530

    vercel config

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect("mongodb+srv://amit:TewTFc2JfjonIcEy@cluster01.4hqcj.mongodb.net/?retryWrites=true&w=majority");
     console.log('Database connected');
   } catch (error) {
     console.log(error.message);
