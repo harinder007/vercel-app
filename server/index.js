035256f71a7f5b72f6c397569b8645f285dd7928
harinder007 <harinderyadav1008@gmail.com>
2023-04-14 19:25:19 +0530

    vercel config

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ app.use(express.json());
 
 connectDB();
 
-// // Define Routes
-// app.use('/api/papers', require('./routes/api/papers'));
-// app.use('/api/programs', require('./routes/api/programs'));
-// app.use('/api/studyMaterial', require('./routes/api/studyMaterial'));
-// app.use('/api/syllabuses', require('./routes/api/syllabuses'));
-// app.use('/api/auth', require('./routes/api/auth'));
+// Define Routes
+app.use('/api/papers', require('./routes/api/papers'));
+app.use('/api/programs', require('./routes/api/programs'));
+app.use('/api/studyMaterial', require('./routes/api/studyMaterial'));
+app.use('/api/syllabuses', require('./routes/api/syllabuses'));
+app.use('/api/auth', require('./routes/api/auth'));
 app.use('/home', home);
 const port = process.env.PORT;
 
